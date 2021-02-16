ad732848e500247f35c2ba1580d5c6b310eeae55
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-16 17:49:29 -0300

    application working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %w[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -17,9 +19,29 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # list all tasks
-  get 'tasks', to: 'tasks#index'
+  # # list all tasks
+  # get 'tasks', to: 'tasks#index'
 
-  # form to create a new task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # # form to create a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
 
-  # show one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # show one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # create a task in our DB
-  post 'tasks', to: 'tasks#create'
+  # # create a task in our DB
+  # post 'tasks', to: 'tasks#create'
+
+  # # show a form to edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # # Update a task on our DB
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete task from our DB
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
