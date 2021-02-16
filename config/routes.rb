74e6f6fe2ee1908922a4d60128dd075499f59db5
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-16 16:29:21 -0300

    create tasks implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # list all tasks
   get 'tasks', to: 'tasks#index'
 
+  # form to create a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # show one task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # create a task in our DB
+  post 'tasks', to: 'tasks#create'
 end
